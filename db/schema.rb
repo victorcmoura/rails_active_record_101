c342d4b94ba2462bf4dbdb5634d4b93f986baba3
Victor Moura <victor_cmoura@hotmail.com>
2018-11-21 23:26:28 -0200

    Adds likes attribute to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181122003356) do
+ActiveRecord::Schema.define(version: 20181122010951) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes"
   end
 
 end
