c61d790f3169742b3b2b66ffa76f9f0d0d9ad671
Victor Moura <victor_cmoura@hotmail.com>
2018-11-21 23:36:47 -0200

    Updates schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181122010951) do
+ActiveRecord::Schema.define(version: 20181122013459) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20181122010951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
