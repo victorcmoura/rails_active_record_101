a097e8e472183825dcfdf5d706bd722823794253
Victor Moura <victor_cmoura@hotmail.com>
2018-11-26 14:44:57 -0200

    Adds articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/myroute'
 
+  get 'articles', to: 'articles#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
